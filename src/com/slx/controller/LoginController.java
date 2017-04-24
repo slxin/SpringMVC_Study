03353e0b08f79be71b8061c1bb337f80d4cee5e0
slxin <1274140131@qq.com>
2017-04-24 20:21:12 +0800

    复习一下springMVC执行流程

---
+++ src/com/slx/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.slx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+@Controller
+public class LoginController {
+    /**
+     * @RequestParam注解的作用是：根据参数名从URL中取得参数值
+     * @param username
+     *            用户名，一定要对应着表单的name才行
+     * @param password
+     *            用户密码，也应该对应表单的数据项
+     * @param model
+     *            一个域对象，可用于存储数据值
+     * @return
+     */
+    @RequestMapping("/login") // @RequestMapping 注解可以用指定的URL路径访问本控制层
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+                        Model model) {
+
+        if (username.equals("admin") && password.equals("admin")) {
+            model.addAttribute("username", username);
+            return "success.jsp";
+        } else {
+            model.addAttribute("username", username);
+            return "fail.jsp";
+        }
+    }
+
+}
